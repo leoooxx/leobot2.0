ecf15a4dea175d1a441f09bff5b76329c47dd2f2
leoooxx <100388043+leoooxx@users.noreply.github.com>
2022-11-09 02:29:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,8 @@ global.linkz = "https://www.youtube.com/channel/UCtrQz2uj9UvoneejlvUkVKw" //your
 global.websitex = "https://www.youtube.com/channel/UCtrQz2uj9UvoneejlvUkVKw" //your website to be displayed
 global.botscript = 'https://github.com/leoooxx/CheemsBot-MD5' //script link
 global.themeemoji = "🦦" //ur theme emoji
-global.packname = "🦄 𝟚.𝟘" //ur sticker watermark packname
+global.packname = "🦄 LEO-BOT
+ 𝟚.𝟘" //ur sticker watermark packname
 global.author = "𝕷𝕰𝕺🦄" //ur sticker watermark author
 global.wm = "𝕃𝔼𝕆-𝔹𝕆𝕋 Inc." //ur watermark
 
